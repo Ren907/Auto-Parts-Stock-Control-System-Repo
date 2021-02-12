bb239744745ae3b6f16b936b438d17c20a57139b
Renald Dalli <renald_dalli@hotmail.com>
2021-02-12 10:18:46 +0100

    First Commit

---
+++ AutoPartsStockControlSystem/AutoPartsStockControlSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoPartsStockControlSystem.Startup))]
+namespace AutoPartsStockControlSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

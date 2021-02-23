c0f003abc6e452e45abfd3c7c3c3c11dab6b0fb6
Renald Dalli <renald_dalli@hotmail.com>
2021-02-23 09:49:20 +0100

    Second Commit: Admin and User pages are created and Layouts that link pages to naviagte.

---
+++ AutoPartsStockControlSystem/AutoPartsStockControlSystem/Controllers/AdminPageController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoPartsStockControlSystem.Controllers
+{
+    public class AdminPageController : Controller
+    {
+        // GET: AdminDashBoard
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        
+        public ActionResult AdminSearchStock()
+        {
+            return View();
+        }
+
+        public ActionResult AdminUsers()
+        {
+            return View();
+        }
+
+        public ActionResult AdminReports()
+        {
+            return View();
+        }
+
+        public ActionResult AdminSuppliers()
+        {
+            return View();
+        }
+
+        public ActionResult AdminSendMessage()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ AutoPartsStockControlSystem/AutoPartsStockControlSystem/Controllers/UserPageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoPartsStockControlSystem.Controllers
+{
+    public class UserPageController : Controller
+    {
+        // GET: UserPage
+        public ActionResult UserDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult UserSearchStock()
+        {
+            return View();
+        }
+
+        public ActionResult UserAddNewItem()
+        {
+            return View();
+        }
+
+        public ActionResult UserUpdateStock()
+        {
+            return View();
+        }
+
+        public ActionResult UserSales()
+        {
+            return View();
+        }
+
+       
+        public ActionResult UserSuppliers()
+        {
+            return View();
+        }
+
+        public ActionResult UserSendMessage()
+        {
+            return View();
+        }
+    }
+}
